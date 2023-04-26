b1a5e0182b8aba5baf0a0265d6944e853f0b295e
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-26 16:19:07 -0300

    feat: exibindo os pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,35 +1,38 @@
 import { AbBotao } from "alura-books-ds-guimarans";
 import axios from "axios";
 import './Pedidos.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IPedido } from "interfaces/IPedido";
 
 const Pedidos = () => {
+    const formatador = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'})
+    const [pedidos, setPedidos] = useState<IPedido[]>([])
 
     useEffect(() => {
         const token = sessionStorage.getItem('token')
-
         axios.get<IPedido[]>('http://localhost:8000/pedidos', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
-            .then(resposta => console.log(resposta.data))
+            .then(resposta => setPedidos(resposta.data))
             .catch(erro => console.error(erro))
-    }, []) 
+    }, [])
     return (
         <section className="pedidos">
             <h1>Pedidos</h1>
-            <div className="pedido">
-                <ul>
-                    <li>Pedido: <strong>89019041</strong></li>
-                    <li>Data do pedido: <strong>26/05/2022</strong></li>
-                    <li>Valor total: <strong>R$ 48,00</strong></li>
-                    <li>Entrega realizada em: <strong>30/0/2022 </strong></li>
-                </ul>
+            {pedidos.map(pedido => (
+                <div className="pedido" key={pedido.id}>
+                    <ul>
+                        <li>Pedido: <strong>{pedido.id}</strong></li>
+                        <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+                        <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
+                        <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+                    </ul>
 
-                <AbBotao tamanho="pequeno" texto="Detalhes" />
-            </div>
+                    <AbBotao tamanho="pequeno" texto="Detalhes" />
+                </div>
+            ))}
         </section>
     )
 }
