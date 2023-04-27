4f7fbbcc0658f35db71c977bea51f808c9160e95
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-27 18:24:59 -0300

    refactor: inclusão do apollo client remoção do useQuery e refatoração do componente de lista livros

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Rotas from './rotas';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ABApolloClient from 'componentes/ABApolloClient';
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ABApolloClient>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Rotas />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ABApolloClient>
   );
 }
 
--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,20 +1,36 @@
-import { useQuery } from "@tanstack/react-query"
 import CardLivro from "componentes/CardLivro"
-import { obterProdutosDaCategoria } from "../../http"
 import { ICategoria } from "interfaces/ICategoria"
 
 import './ListaLivros.css'
+import { gql, useQuery } from "@apollo/client"
+import { ILivro } from "interfaces/iLivro"
 
 interface ListaLivrosProps {
     categoria: ICategoria
 }
 
+const OBTER_LIVROS = gql`
+    query ObterLivros {
+        livros {
+            id
+            slug
+            titulo
+            imagemCapa
+            opcoesCompra {
+                id
+                preco
+            }
+        }
+    }
+`
+
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
-    const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
+
+    const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS)
 
     return (
         <section className="ListaLivros">
-            {produtos?.map(livro => <CardLivro livro={livro} key={livro.id} /> )}
+            {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
         </section>
 
     )
