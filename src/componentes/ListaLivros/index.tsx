0f028d37ff3f2be33095504e9a10f479e1beead5
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-27 18:44:39 -0300

    feat: layout da busca por titulo

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -4,6 +4,8 @@ import { ICategoria } from "interfaces/ICategoria"
 import './ListaLivros.css'
 import { gql, useQuery } from "@apollo/client"
 import { ILivro } from "interfaces/iLivro"
+import { AbBotao, AbCampoTexto } from "alura-books-ds-guimarans"
+import { useState } from "react"
 
 interface ListaLivrosProps {
     categoria: ICategoria
@@ -26,6 +28,8 @@ const OBTER_LIVROS = gql`
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
+    const [textoBusca, setTextoBusca] = useState('')
+
     const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS, {
         variables: {
             categoriaId: categoria.id
@@ -33,8 +37,16 @@ const ListaLivros = ({ categoria }: ListaLivrosProps) => {
     })
 
     return (
-        <section className="ListaLivros">
-            {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+        <section>
+            <form style={{ maxWidth: '60%', margin: '0 auto', textAlign: 'center'}}>
+                <AbCampoTexto value={textoBusca} onChange={setTextoBusca} placeholder="Digite o titulo" />
+                <div>
+                    <AbBotao tamanho="pequeno" texto="Buscar" />
+                </div>
+            </form>
+            <div className="ListaLivros">
+                {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+            </div>
         </section>
     )
 }
