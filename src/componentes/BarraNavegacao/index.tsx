2dbdf86440c69fcf4046cd8033765c37172ce908
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-27 19:07:18 -0300

    refactor: lista de categorias com apollo client

--- src/componentes/BarraNavegacao/index.tsx
+++ src/componentes/BarraNavegacao/index.tsx
@@ -4,23 +4,27 @@ import logo from './assets/logo.png'
 import usuario from './assets/usuario.svg'
 import './BarraNavegacao.css'
 import ModalCadastroUsuario from "componentes/ModalCadastroUsuario"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import ModalLoginUsuario from "componentes/ModalLoginUsuario"
+import { gql, useQuery } from "@apollo/client"
 import { ICategoria } from "interfaces/ICategoria"
-import http from "../../http"
+
+const OBTER_CATEGORIAS = gql `
+    query ObterCategorias {
+        categorias {
+            id
+            nome
+            slug
+        }
+    }
+`
 
 const BarraNavegacao = () => {
 
     const [modalCadastroAberta, setModalCadastroAberta] = useState(false)
     const [modalLoginAberta, setModalLoginAberta] = useState(false)
 
-    const [categorias, setCategorias] = useState<ICategoria[]>([])
-
-    useEffect(() => {
-        http.get<ICategoria[]>('/categorias')
-            .then(resposta => setCategorias(resposta.data))
-            .catch(error => console.error(error))
-    }, [])
+    const { data } = useQuery<{ categorias: ICategoria[] }>(OBTER_CATEGORIAS)
 
     const navigate = useNavigate();
 
@@ -51,7 +55,7 @@ const BarraNavegacao = () => {
                 <li>
                     <a href="#!"> Categorias </a>
                     <ul className="submenu">
-                        {categorias.map(categoria => (
+                        {data?.categorias.map(categoria => (
                             <li key={categoria.id}>
                                 <Link to={`/categorias/${categoria.slug}`}>
                                     {categoria.nome}
