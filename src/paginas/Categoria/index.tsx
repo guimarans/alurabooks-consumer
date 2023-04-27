0d028b644edae36cea0549df2b76c86620359fa5
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-27 11:39:15 -0300

    feat: desenvolvimento do loader e inclusão do mesmo na requisição de categoria

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -3,21 +3,32 @@ import { ICategoria } from "interfaces/ICategoria";
 import { useEffect, useState } from "react";
 import http from '../../http'
 import { useParams } from "react-router-dom";
+import Loader from "componentes/Loader";
 
 const Categoria = () => { 
 
     const [categoria, setCategoria] = useState<ICategoria>()
+    const [carregando, setCarregando] = useState(true)
+    
     const params = useParams()
 
     useEffect(() => {
+        setCarregando(true)
         http.get<ICategoria[]>('/categorias', {
             params: {
                 slug: params.slug
             }
         })
-            .then(resposta => setCategoria(resposta.data[0]))
+            .then(resposta => {
+                setCategoria(resposta.data[0])
+                setCarregando(false)
+            })
             .catch(error => console.error(error))
     }, [params.slug])
+
+    if(carregando) {
+        return <Loader />
+    }
  
     return (
         <section>
